63ff9730a2ce17a154211a9affb065a000620ed0
Florin Leotescu <florin.leotescu@nxp.com>
2024-03-11 11:19:17 +0200

    LF-11699-3 tcm: refactor TCM init
    Refactor TCM OEI by using EDMA driver.
    
    Signed-off-by: Florin Leotescu <florin.leotescu@nxp.com>

--- source/tcm/main.c
+++ source/tcm/main.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  */
 #include <asm/arch/clock.h>
 #include <time.h>
@@ -14,6 +14,9 @@ extern int tcm_init(void);
 uint32_t __attribute__((section(".entry"))) oei_entry(void)
 {
 	int ret = 0;
+#ifdef DEBUG
+	unsigned int ts, te;
+#endif
 
 	if (!timer_is_enabled())
 		timer_enable();
@@ -23,7 +26,15 @@ uint32_t __attribute__((section(".entry"))) oei_entry(void)
 	lpuart32_serial_init();
 
 	printf("\n\n** TCM OEI: start **\n");
+#ifdef DEBUG
+	ts = timer_get_us();
+#endif
 	ret = tcm_init();
+
+#ifdef DEBUG
+	te = timer_get_us() - ts;
+	printf("** TCM INIT: %u us **\n", te);
+#endif
 	printf("\n\n** TCM OEI: end ret = 0x%x **\n", ret);
 
 	return (ret == 0 ? OEI_SUCCESS_FREE_MEM : OEI_FAIL);
