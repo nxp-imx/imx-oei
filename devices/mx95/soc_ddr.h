1a572a640ef8d6883e8ca39744cd6d2d5dbed678
Viorel Suman <viorel.suman@nxp.com>
2024-05-31 17:53:22 +0300

    LF-12554-2 ddr: change training data load location
    Change the training data load location to last 0x10000 bytes
    in OCRAM at 0x204c8000 because 0x3001C000 overlaps with ROM
    TCM area used for DMA transfer from EMMC in EMMC fastboot mode.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

--- devices/mx95/soc_ddr.h
+++ devices/mx95/soc_ddr.h
@@ -7,7 +7,7 @@
 
 #include "ddr.h"
 
-#define QB_STATE_LOAD_ADDR	0x3001C000	/** QB Flow: training data load location, SysTCM, ends at 0x3002DFFF */
+#define QB_STATE_LOAD_ADDR	0x204c8000	/** QB Flow: training data load location, end of OCRAM */
 #define QB_STATE_LOAD_SIZE	0x10000		/** QB Flow: hole size, 64k */
 #define QB_STATE_SAVE_ADDR	0x4aaf4000	/** TR Flow: training data save location */
 
