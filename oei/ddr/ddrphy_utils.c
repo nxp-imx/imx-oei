nxp-imx/imx-oei

commit 6b62848f64fbf7893eef80f183e057990b5b8e6c

ddr: optimize the cases for drate divisible to 8
Optimize the cases when the remainder of the drate
divided by 8 is zero.

Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

oei/ddr/ddrphy_utils.c
@@ -106,43 +106,19 @@ void Ddr_Phy_Init_Set_Dfi_Clk(unsigned int drate)
     switch (drate)
     {
     case 6400:
-        Dram_PLL_Init(MHZ(800));
-        Dram_Disable_Bypass();
-        break;
     case 4800:
-        Dram_PLL_Init(MHZ(600));
-        Dram_Disable_Bypass();
-        break;
     case 4266: /* Assume 4266 */
-        Dram_PLL_Init(533250000);
-        Dram_Disable_Bypass();
-        break;
     case 4000:
-        Dram_PLL_Init(MHZ(500));
-        Dram_Disable_Bypass();
-        break;
     case 3733: /* Assume 3733 */
-        Dram_PLL_Init(466625000);
-        Dram_Disable_Bypass();
-        break;
     case 3200:
-        Dram_PLL_Init(400000000);
-        Dram_Disable_Bypass();
-        break;
     case 2133:
-        Dram_PLL_Init(266625000);
-        Dram_Disable_Bypass();
-        break;
     case 1866: /* Assume 1866 */
-        Dram_PLL_Init(233250000);
-        Dram_Disable_Bypass();
-        break;
     case 1600:
-        Dram_PLL_Init(200000000);
+        Dram_PLL_Init(MHZ(drate) >> 3); /** drate/8 */
         Dram_Disable_Bypass();
         break;
     default:
-        printf("Clk rate not found\n");
+        printf("Clk rate %u not found\n", drate);
         return;
     }
 }

commit c5da12ae10fd41395419d9178a50a20fc5489f87

ddr: expand the rate to uint64_t before further calculation
Expand the rate to uint64_t before further calculation.
The issue happens for 6400MTS.

Fixes: 6b62848 ("ddr: optimize the cases for drate divisible to 8")
Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

oei/ddr/ddrphy_utils.c
@@ -103,6 +103,10 @@ int Wait_Ddr_Phy_Training_Complete(void)
 
 void Ddr_Phy_Init_Set_Dfi_Clk(unsigned int drate)
 {
+    uint64_t rate = (uint64_t)drate;
+
+    rate *= 125000ULL; /** multiply to 1000000 (in MHz) and divide by 8 */
+
     switch (drate)
     {
     case 6400:
@@ -114,7 +118,7 @@ void Ddr_Phy_Init_Set_Dfi_Clk(unsigned int drate)
     case 2133:
     case 1866: /* Assume 1866 */
     case 1600:
-        Dram_PLL_Init(MHZ(drate) >> 3); /** drate/8 */
+        Dram_PLL_Init(rate); /** drate/8 */
         Dram_Disable_Bypass();
         break;
     default: