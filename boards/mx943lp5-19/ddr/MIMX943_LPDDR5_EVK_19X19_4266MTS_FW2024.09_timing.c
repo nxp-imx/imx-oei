fcae4dc6633e697e18f712d20ad04d38aa657c69
Viorel Suman <viorel.suman@nxp.com>
2025-05-09 15:52:43 +0300

    boards: mx943lp5-19: disable SSC by default
    Disable SSC by default.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

--- boards/mx943lp5-19/ddr/MIMX943_LPDDR5_EVK_19X19_4266MTS_FW2024.09_timing.c
+++ boards/mx943lp5-19/ddr/MIMX943_LPDDR5_EVK_19X19_4266MTS_FW2024.09_timing.c
@@ -10714,6 +10714,7 @@ static struct dram_fsp_msg ddr_dram_fsp_msg[] = {
     {
         /* P0 4266mbps */
         .drate = 4266,
+        .ssc = false,
         .fsp_phy_cfg = ddr_phy_fsp0_cfg,
         .fsp_phy_cfg_num = ARRAY_SIZE(ddr_phy_fsp0_cfg),
         .fw_type = FW_1D_IMAGE,
