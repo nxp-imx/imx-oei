08214a02ca6df65b38a002f0cee6432874c709cc
Viorel Suman <viorel.suman@nxp.com>
2026-05-11 13:38:18 +0300

    boards: mx95lp5-19: blank sync timings update to latest DDR Tool
    Blank sync timing update to DDR Tool v4.1.0_1.3-bbe174500.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

--- boards/mx95lp5/ddr/MIMX95_LPDDR5_EVK_19X19_6400MTS_FW2024.09_ECC_enabled_timing.c
+++ boards/mx95lp5/ddr/MIMX95_LPDDR5_EVK_19X19_6400MTS_FW2024.09_ECC_enabled_timing.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Code generated with DDR Tool v4.0.0_1.2-ac07e4ce6.
+ * Code generated with DDR Tool v4.1.0_1.3-bbe174500.
  * DDR PHY FW2024.09-SP2
  * Chip revision: B0
  * Part number: NXP LPDDR5 EVK 19x19 board's default DDR part
