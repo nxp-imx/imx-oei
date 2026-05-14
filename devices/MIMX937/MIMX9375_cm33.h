33df8c52e2b3602b272ac9c36313c4eb45243571
Viorel Suman <viorel.suman@nxp.com>
2026-05-14 10:05:57 +0300

    devices: add MIMX937 support
    Add MIMX937 device support.
    
    Signed-off-by: Viorel Suman <viorel.suman@nxp.com>

---
+++ devices/MIMX937/MIMX9375_cm33.h
@@ -0,0 +1,57 @@
+/*
+** ###################################################################
+**     Processors:          MIMX9375xxVTx_cm33
+**                          MIMX9375xxVZx_cm33
+**
+**     Compilers:           GNU C Compiler
+**                          IAR ANSI C/C++ Compiler for ARM
+**                          Keil ARM C/C++ Compiler
+**
+**     Reference manual:    iMX937RM Rev. 1 Draft A
+**     Version:             rev. 1.0, 2026-04-09
+**     Build:               b260522
+**
+**     Abstract:
+**         CMSIS Peripheral Access Layer for MIMX9375_cm33
+**
+**     Copyright 1997-2016 Freescale Semiconductor, Inc.
+**     Copyright 2016-2026 NXP
+**     SPDX-License-Identifier: BSD-3-Clause
+**
+**     http:                 www.nxp.com
+**     mail:                 support@nxp.com
+**
+**     Revisions:
+**     - rev. 1.0 (2026-04-09)
+**         Initial version.
+**
+** ###################################################################
+*/
+
+/*!
+ * @file MIMX9375_cm33.h
+ * @version 1.0
+ * @date 2026-04-09
+ * @brief CMSIS Peripheral Access Layer for MIMX9375_cm33
+ *
+ * CMSIS Peripheral Access Layer for MIMX9375_cm33
+ */
+
+#if !defined(MIMX9375_cm33_H_)  /* Check if memory map has not been already included */
+#define MIMX9375_cm33_H_
+
+/* IP Header Files List */
+#include "PERI_BLK_CTRL_S_AONMIX.h"
+#include "PERI_BBNSM.h"
+#include "PERI_CCM.h"
+#include "PERI_DDR_BLK_CTRL_DDRMIX.h"
+#include "PERI_DDRC.h"
+#include "PERI_LPUART.h"
+#include "PERI_MU.h"
+#include "PERI_PLL.h"
+#include "PERI_SRC_GEN.h"
+#include "PERI_SRC_MEM.h"
+#include "PERI_SRC_XSPR.h"
+#include "PERI_SYS_CTR_CONTROL.h"
+
+#endif  /* #if !defined(MIMX9375_cm33_H_) */
